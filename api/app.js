6b7aff6888b2c0a7ab8a9289c450107db318341c
FliprInternAlfaiz <mrangrez@flipr.ai>
2025-08-29 14:52:20 +0530

    changes complete code

--- api/app.js
+++ api/app.js
@@ -16,6 +16,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend API is running...");
+});
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
